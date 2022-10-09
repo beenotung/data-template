22d417bd8fd9dcd9182a60160a3adbc010639be5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-10-09 12:39:51 +0800

    expose helper functions to cache data from ajax

--- public/base.js
+++ public/base.js
@@ -24,13 +24,24 @@
       }
     })
 
-  let loadTemplate = async name => {
-    let html = localStorage.getItem(name)
-    let p = fetch(name).then(res => res.text())
-    if (!html) {
-      html = await p
-    }
-    p.then(html => localStorage.setItem(name, html))
+  window.getText = (url, options) => {
+    let text = localStorage.getItem(url)
+    let p = fetch(url, options).then(res => res.text())
+    p.then(text => localStorage.setItem(url, text))
+    return text || p
+  }
+
+  window.getJSON = (url, options) => {
+    let text = getText(url, options)
+    return text.then ? text.then(JSON.parse) : JSON.parse(text)
+  }
+
+  let loadTemplate = name => {
+    let html = getText(name)
+    return html.then ? html.then(createTemplate) : createTemplate(html)
+  }
+
+  let createTemplate = html => {
     let template = document.createElement(t)
     // deepcode ignore DOMXSS: the template is authored by the application developer, not from untrusted users
     template.innerHTML = html
