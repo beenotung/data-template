73cfa87b66f92062ec19f1069c3db72ed9772ef7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-10-30 14:48:26 +0800

    copy the base files to top level for CDN path

--- scripts/update-hash.sh
+++ scripts/update-hash.sh
@@ -2,5 +2,7 @@
 set -e
 set -o pipefail
 ./scripts/size.sh
+cp template/public/base.js .
+cp template/public/base.min.js .
 ./scripts/hash.sh base.js >> README.md
 ./scripts/hash.sh base.min.js >> README.md
